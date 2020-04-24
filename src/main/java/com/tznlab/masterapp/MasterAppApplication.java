2b53e66401ad0dcd670e5086f3dbede32a1affcf
Yasir Hussain <35362824+yHussain09@users.noreply.github.com>
2020-04-24 15:56:04 +0500

    Initial commit

---
+++ src/main/java/com/tznlab/masterapp/MasterAppApplication.java
@@ -0,0 +1,13 @@
+package com.tznlab.masterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterAppApplication.class, args);
+	}
+
+}
